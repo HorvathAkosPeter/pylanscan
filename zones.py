6814ecde2ab97b3a3421c6b8795b16b0b19c9c6d
Peter Horvath <horvath.akos.peter@gmail.com>
2025-12-31 15:54:31 +0100

    zoneslistorubstness

--- zones.py
+++ zones.py
@@ -4,16 +4,21 @@ import subprocess
 import ddns_store
 import config
 
+from lib import die
+
 ddns_cfg = []
 
 for e in dir(config):
   i = getattr(config, e)
   try:
-    if "store_type" in i and i["store_type"] == ddns_store:
+    if "store_type" in i and i["store_type"] == "ddns_store":
       ddns_cfg = i
       break
   except TypeError:
     pass
+
+if not ddns_cfg:
+  die ("no ddns_store in configured entities")
 
 for zone in [ddns_cfg["fwd_zone"], ddns_cfg["rev_zone"]]:
   cmd = ["host", "-4", "-T", "-p", str(ddns_cfg["srv_port"]), "-v", "-t", "AXFR", zone + ".", ddns_cfg["srv_host"]]
